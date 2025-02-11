f8c4d4631818b66296a75cf5ecb63bf757440eb8
gretafg <gretafg@al.insper.edu.br>
2025-02-11 18:40:41 -0300

    ex 2

--- exe2/main.c
+++ exe2/main.c
@@ -3,6 +3,7 @@
 #include <stdio.h>
 
 const int BTN_PIN = 26;
+const int BTN_PIN_BLUE = 7;
 
 int main() {
   stdio_init_all();
@@ -11,10 +12,20 @@ int main() {
   gpio_set_dir(BTN_PIN, GPIO_IN);
   gpio_pull_up(BTN_PIN);
 
+  gpio_init(BTN_PIN_BLUE);
+  gpio_set_dir(BTN_PIN_BLUE, GPIO_IN);
+  gpio_pull_up(BTN_PIN_BLUE);
+
   while (true) {
     if (!gpio_get(BTN_PIN)) {
       printf("Botao 1\n");
       while (!gpio_get(BTN_PIN)) {
+      };
+    }
+
+    if (!gpio_get(BTN_PIN_BLUE)) {
+      printf("Botao 2\n");
+      while (!gpio_get(BTN_PIN_BLUE)) {
       };
     }
   }
